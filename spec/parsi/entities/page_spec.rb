f1c07edb038275b692c81261256a98b208768abb
Aleksey Markov <amrkv@yandex.ru>
2020-02-06 00:49:24 +0300

    Add Page entity spec

--- spec/parsi/entities/page_spec.rb
+++ spec/parsi/entities/page_spec.rb
@@ -1,3 +1,13 @@
 RSpec.describe Page, type: :entity do
-  # place your tests here
+  it 'can be initialized with attributes' do
+    page = Page.new(
+      url: 'guides.hanamirb.org',
+      title: 'Hanami guides',
+      status: 'processed'
+    )
+
+    expect(page.url).to eq('guides.hanamirb.org')
+    expect(page.title).to eq('Hanami guides')
+    expect(page.status).to eq('processed')
+  end
 end
