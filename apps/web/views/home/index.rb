dbfac52c7be9da6e83a8ff9711f2f354b656c601
Aleksey Markov <a.markov@itransition.com>
2020-02-07 14:33:09 +0300

    Refactor View label

--- apps/web/views/home/index.rb
+++ apps/web/views/home/index.rb
@@ -3,6 +3,14 @@ module Web
     module Home
       class Index
         include Web::View
+
+        def status_label(page)
+          raw %(
+            <span class="uk-label uk-label-#{page.status == 'processed' ? 'success' : 'danger'}">
+              #{page.status}
+            </span>
+          )
+        end
       end
     end
   end
