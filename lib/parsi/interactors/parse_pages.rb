ef10b3084c15e9700cd2e61b26d5a6e1acb278ff
Aleksey Markov <a.markov@itransition.com>
2020-02-07 16:50:20 +0300

    Add code notations

--- lib/parsi/interactors/parse_pages.rb
+++ lib/parsi/interactors/parse_pages.rb
@@ -1,5 +1,5 @@
 require 'hanami/interactor'
-# NOTE: We're using standard Ruby library for page fetches instead third-party ones
+# NOTE: We're using standard Ruby library for page fetches instead of third-party ones
 require 'open-uri'
 
 class ParsePages
@@ -11,13 +11,17 @@ class ParsePages
 
   def call(pages_attributes)
     return unless pages_attributes
-    # NOTE: Filter out unique URLs
+    # NOTE: Filter out duplicate URLs
     pages_attributes.uniq!
 
     threads = []
     fetched_pages = []
 
     for page_to_fetch in pages_attributes
+      # NOTE: We're using Ruby Threads instead of third-party libraries for demo purposes
+      #   For each URL we create new thread. Using too many threads can destroy system resources
+      #   To keep things simple, we won't implement our own Thread Pool
+      #   For Production consider using libraries like Celluloid, Concurrent Ruby, Parallel, etc. with built-in Thread Pool
       threads << Thread.new(page_to_fetch) do |url|
         puts "Fetching: #{url}"
 
@@ -29,9 +33,15 @@ class ParsePages
       end
     end
 
+    # NOTE: Wait for threads to finish
     threads.each {|thr| thr.join }
 
     puts fetched_pages
+
+    # NOTE: To keep things simple, we overwrite pages history on each request
+    #   In a real-life service, we should deal with duplicates
+    #   For example, we may use upsert (INSERT ON CONFLICT for PostgreSQL)
+    #   https://www.postgresqltutorial.com/postgresql-upsert/
     @repository.clear
     @pages = @repository.create_many(fetched_pages)
   end
--- lib/parsi/repositories/page_repository.rb
+++ lib/parsi/repositories/page_repository.rb
@@ -1,4 +1,6 @@
 class PageRepository < Hanami::Repository
+  # NOTE: Create records in bulk
+  #   see more: https://guides.hanamirb.org/repositories/overview/
   def create_many(data)
     command(create: :pages, result: :many).call(data)
   end
